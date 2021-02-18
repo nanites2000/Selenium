af7c17c9b4e8652baa70ce4bb269d7ae7f89451e
Gerrit Larsen <nanites2000@yahoo.com>
2021-02-18 01:06:22 -0700

    Iinital commit- learning selenium

---
+++ first.py
@@ -0,0 +1,27 @@
+from seleniumbase import BaseCase
+import time
+class MyTestClass(BaseCase):
+
+    def test_basics(self):
+        # url = "https://store.xkcd.com/collections/posters"
+        # self.open(url)
+        # self.type('input[name="q"]', "xkcd book")
+        # self.click('input[value="Search"]')
+        # self.assert_text("xkcd: volume 0", "h3")
+        # self.open("https://xkcd.com/353/")
+        # self.assert_title("xkcd: Python")
+        # self.assert_element('img[alt="Python"]')
+        # self.click('a[rel="license"]')
+        # self.assert_text("free to copy and reuse")
+        # self.go_back()
+        # self.click_link("About")
+        # self.assert_exact_text("xkcd.com", "h2")
+        url = "https://engadget.com"
+        self.open(url)
+        time.sleep(3)
+        self.assert_title("Engadget")
+        self.click('button#searchBtn')
+
+        self.type('input#search-terms', "Bobby mcBob Face\r\n")
+        self.assert_title('Bobby mcBob Face - Engadget Search Results')
+        time.sleep(4)
